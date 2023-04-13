89af13a455e708513e39ef839ca81765c80075fa
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-04-14 00:14:49 +0300

    Adds contacts to LocalStorage.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,23 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    if (localContacts) {
+      this.setState({ contacts: JSON.parse(localContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contact !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   changeFilter = e => {
     this.setState({ filter: e.target.value });
   };
@@ -43,8 +52,8 @@ export class App extends Component {
   deleteContact = id => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== id),
+      filter: '',
     }));
-    this.setState({ filter: '' });
   };
 
   render() {
